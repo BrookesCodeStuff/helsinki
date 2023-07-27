b131ee3854519b5f8ea5662198c8d1a0487678d3
BrookesCodeStuff <brookepaglia@gmail.com>
2023-07-27 09:46:26 -0400

    refactor Content component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import Content from './components/Content';
 
 const App = () => {
   const course = 'Half Stack Application Development';
@@ -12,17 +13,15 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
 
-      <p>
-        {part2} {exercises2}
-      </p>
-
-      <p>
-        {part3} {exercises3}
-      </p>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
