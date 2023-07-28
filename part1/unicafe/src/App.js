81762d1f60e587436a08f6a287e93291065996a7
BrookesCodeStuff <brookepaglia@gmail.com>
2023-07-27 22:05:59 -0400

    initial setup

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <h1>give feedback</h1>
+
+      <h1>statistics</h1>
+    </>
+  );
+};
+
+export default App;
